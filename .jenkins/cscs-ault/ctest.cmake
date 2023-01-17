f657e9a411604926e7674ef30903081942e4c3bb
aurianer <aurianer@cscs.ch>
2023-01-17 17:01:49 +0100

    Make use of test_args, otherwise unused

--- .jenkins/cscs-ault/ctest.cmake
+++ .jenkins/cscs-ault/ctest.cmake
@@ -81,7 +81,7 @@ ctest_build(TARGET all FLAGS "-k0 ${CTEST_BUILD_EXTRA_OPTIONS}")
 ctest_build(TARGET examples FLAGS "-k0 ${CTEST_BUILD_EXTRA_OPTIONS}")
 ctest_build(TARGET tests FLAGS "-k0 ${CTEST_BUILD_EXTRA_OPTIONS}")
 ctest_submit(PARTS Build)
-ctest_test(PARALLEL_LEVEL "${CTEST_TEST_PARALLELISM}")
+ctest_test(${test_args})
 ctest_submit(PARTS Test BUILD_ID CTEST_BUILD_ID)
 file(WRITE "${CTEST_JOB_NAME}-cdash-build-id.txt"
      "${CTEST_BUILD_ID}"
