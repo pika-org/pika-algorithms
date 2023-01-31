d3b7ec6feb93598cd39d3a5363c70950afc2eb4a
Mikael Simberg <mikael.simberg@iki.fi>
2023-01-31 15:29:36 +0100

    Require pika 0.12.0

--- cmake/pika_algorithms_setup_pika.cmake
+++ cmake/pika_algorithms_setup_pika.cmake
@@ -4,7 +4,7 @@
 # Distributed under the Boost Software License, Version 1.0. (See accompanying
 # file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-pika_algorithms_find_package(pika 0.11.0 REQUIRED)
+pika_algorithms_find_package(pika 0.12.0 REQUIRED)
 
 if(NOT PIKA_ALGORITHMS_FIND_PACKAGE)
   target_link_libraries(pika_algorithms_base_libraries INTERFACE pika::pika)
